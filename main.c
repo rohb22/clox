9004a0997230b73e6c0e52aa4c665a88749e8870
John Rohb Paloma <palomajohnrohb@gmail.com>
2025-01-14 10:57:37 +0800

    initialized vm

--- main.c
+++ main.c
@@ -1,24 +1,37 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char* argv[]) {
     // For testing
+
+    initVM();
+
     Chunk chunk;
     initChunk(&chunk);
-
-
+    
+    
     writeConstant(&chunk, 1.2, 123);
+    writeConstant(&chunk, 3.4, 123);
+
+
+    writeChunk(&chunk, OP_ADD, 123);
+
+    writeConstant(&chunk, 5.6, 123);
+
+
+    writeChunk(&chunk, OP_DIVIDE, 123);
+
+    writeChunk(&chunk, OP_NEGATE, 123);
 
     writeChunk(&chunk, OP_RETURN, 123);
 
-    writeConstant(&chunk, 1324115151.1412414, 123);
 
-    for (int i = 0; i < 300; i++) {
-    writeConstant(&chunk, i, 123); 
-}
 
-    disassembleChunk(&chunk, "test chunk");
+    disassembleChunk(&chunk, " test chunk ");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
 
 }
