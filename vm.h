4feed028978c95f3a1cb1df03ddd36c7c17f5f6d
John Rohb Paloma <palomajohnrohb@gmail.com>
2025-01-14 11:34:39 +0800

    dynamic stack

--- vm.h
+++ vm.h
@@ -4,13 +4,14 @@
 #include "chunk.h"
 #include "value.h"
 
-#define STACK_MAX 256
 
 typedef struct {
     Chunk* chunk;
     uint8_t* ip;
-    Value stack[STACK_MAX];
+    Value* stack;
     Value* stackTop;
+    int stackCapacity;
+    int stackCount;
 } VM;
 
 typedef enum {
